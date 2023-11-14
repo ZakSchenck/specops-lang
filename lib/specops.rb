d04b6648c17b5be06284222964e5916e3e7c4931
ZakSchenck <zackschenck@gmail.com>
2023-11-13 22:02:48 -0500

    first commit

---
+++ lib/specops.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "specops/version"
+
+module Specops
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/specops_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Specops do
+  it "has a version number" do
+    expect(Specops::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
